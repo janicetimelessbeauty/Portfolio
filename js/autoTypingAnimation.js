5d87c85a92f1b2cd6249af8e62f8e49dbc7e7023
janicetimelessbeauty <nga08102004@gmail.com>
2024-07-01 23:13:00 +1000

    social profiles

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
   $('.about-info-inner').typed({
     strings: [
-      "<span>I'm an aspiring computer science graduate having experience in mining simulator software and web application development with automated testing to assure quality. I enjoy collaborating in Agile teams tomanage Scrum projects throughout the software development cycle. As a key member in previous working teams, I demonstrated a growth mindset in adopting new technologies and an open-minded attitude to innovative perspectives."
+      "<span>I'm an aspiring computer science graduate having experience in equipment training software and software application development with automated testing to assure quality. I enjoy collaborating in Agile teams to manage Scrum projects throughout the software development cycle. As a key member in previous working teams, I demonstrated a growth mindset in adopting new technologies and an open-minded attitude to innovative perspectives."
     ],
     typeSpeed: 7,
     contentType: 'html'
